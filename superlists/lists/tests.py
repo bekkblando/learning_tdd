b882ae09e1a7cdf718b2d81992504c1205cd4dd1
Bekk Blando <bekkblando@gmail.com>
2015-08-22 10:55:25 -0400

    Url link, dummy view works, test runs

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,9 +1,11 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
-
+from lists.views import home_page
 # Create your tests here.
 
 
 class SmokeTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resovles_to_homepage_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
